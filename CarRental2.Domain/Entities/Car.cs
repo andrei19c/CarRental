d1cc523eb0720b0794e147e7b8f2fd04ece868ca
Andrei Ciuciu <andrei.ciuciu@mindgeek.com>
2021-03-27 08:42:17 +0200

    add EF

---
+++ CarRental2.Domain/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CarRental2.Domain
+{
+    public partial class Car
+    {
+        public Guid Id { get; set; }
+        public string Manufacture { get; set; }
+        public string Model { get; set; }
+        public short Year { get; set; }
+        public string Vin { get; set; }
+        public string Description { get; set; }
+        public string Observation { get; set; }
+    }
+}
---
+++ CarRental2.Domain/Entities/CarRentalContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CarRental2.Domain
+{
+    public partial class CarRentalContext : DbContext
+    {
+        public CarRentalContext()
+        {
+        }
+
+        public CarRentalContext(DbContextOptions<CarRentalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost,1433;Database=CarRentalDb;user=SA;password=andrei2013;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("Car");
+
+                entity.Property(e => e.Description).HasMaxLength(500);
+
+                entity.Property(e => e.Manufacture)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Model)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Observation).HasMaxLength(500);
+
+                entity.Property(e => e.Vin)
+                    .IsRequired()
+                    .HasMaxLength(13)
+                    .HasColumnName("VIN");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
